9bdaa05fe804e2822ec72791c1eea773dc92368d
Sarthak Mittal <sam.mittal.2106@gmail.com>
2021-05-28 13:48:26 +0530

    viewing stories and button loader

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const { chromium } = require("playwright-chromium");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const upload = multer();
-var story = require(__dirname + "/models/story");
+const story = require(__dirname + "/models/story"); //dtabase model
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(__dirname + "/public"));
 
+//connecting to database
 mongoose.connect(
   "mongodb+srv://appy:appypass@cluster0.mzeud.mongodb.net/insta-scrapper?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
@@ -17,27 +18,34 @@ mongoose.connect(
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
+//function to open a headless browser and scrape instagram, save stories to database
 async function scrape(userName, pswd) {
+  //open browser
   const browser = await chromium.launch({
-    args: ["--no-sandbox --proxy-server=http://122.155.165.191:3128"],
+    args: ["--no-sandbox"],
   });
+
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("https://www.instagram.com/accounts/login/", {
-    timeout: 60000,
-    waitUntil: "domcontentloaded",
-  });
+  await page.goto("https://www.instagram.com/accounts/login/");
+  //take 1st screen shot
   await page.screenshot({
-    path: `ig-sign-in.png`,
+    path: `./public/snapshots/ig-sign-in.png`,
   });
-  await page.waitForSelector("[type=submit]", {
-    state: "visible",
-    timeout: 60000,
-  });
-  // You can also take screenshots of pages
+  //check if the right page loads
+  try {
+    await page.waitForSelector("[type=submit]", {
+      state: "visible",
+    });
+  } catch (e) {
+    throw new Error("Ip blocked by Instagram sentry");
+  }
+
+  // Take second screen shot
   await page.screenshot({
-    path: `ig-sign-in.png`,
+    path: `./public/snapshots/ig-sign-in.png`,
   });
+  //logging in
   await page.type("[name=username]", userName); // ->
   await page.type('[type="password"]', pswd); // ->
 
@@ -45,7 +53,6 @@ async function scrape(userName, pswd) {
   try {
     await page.waitForSelector("[placeholder=Search]", {
       state: "visible",
-      timeout: 60000,
     });
   } catch (e) {
     throw new Error("Invalid User Name or Password");
@@ -54,7 +61,7 @@ async function scrape(userName, pswd) {
   await page.goto("https://www.instagram.com/", {
     state: "visible",
     timeout: 60000,
-  }); // ->
+  });
   await page.waitForSelector("div.Fd_fQ", {
     state: "visible",
     timeout: 60000,
@@ -62,15 +69,16 @@ async function scrape(userName, pswd) {
 
   await page.click("div.EcJQs", { position: { x: 60, y: 40 } });
 
-  // Execute code in the DOM
   await page.waitForSelector("div.qbCDp img", {
     state: "visible",
     timeout: 60000,
   });
-  await page.screenshot({ path: `profile.png` });
+  //Take snap shot after opening stories
+  await page.screenshot({ path: `./public/snapshots/profile.png` });
   let url = page.url();
   console.log(url);
   let stories = 0;
+  //This loop checks if story is prsent and if true saves that story to database
   while (
     url != `https://www.instagram.com/` &&
     url != "https://www.instagram.com/" + userName
@@ -78,7 +86,6 @@ async function scrape(userName, pswd) {
     let data = await page.evaluate(() => {
       const video = document.querySelectorAll("video.y-yJ5 source");
       if (Object.keys(video) != 0) {
-        console.log(video);
         const urls = Array.from(video).map((v) => v.src);
         return urls;
       }
@@ -86,6 +93,7 @@ async function scrape(userName, pswd) {
       const urls = Array.from(images).map((v) => v.src);
       return urls;
     });
+    //saving to database
     story.create(
       { userName: userName, storyURL: data[0] },
       function (err, story_inst) {
@@ -101,37 +109,73 @@ async function scrape(userName, pswd) {
     url = page.url();
     console.log(url);
   }
-
-  //   await page.waitForTimeout(5000);
+  console.log(stories + " stories added");
   await browser.close();
 
   return stories;
-  // Return the data in form of json
 }
+
+//root
 app.get("/", function (request, response) {
   response.sendFile("./public/index.html");
 });
+
+//after stories are added
 app.get("/added", function (request, response) {
   response.sendFile("./public/added.html", { root: __dirname });
 });
+
+//error page
 app.get("/error", function (request, response) {
   response.sendFile("./public/error.html", { root: __dirname });
 });
+
+//to view snapshot
 app.get("/pic1", function (request, response) {
-  response.sendFile("ig-sign-in.png", { root: __dirname });
+  response.sendFile("./public/snapshots/ig-sign-in.png", { root: __dirname });
 });
+
+//link to added stories
+app.get("/stories", async (request, response) => {
+  const data = await story.find({});
+  let res = "<h2>Link to the added stories</h1>";
+  let index = 0;
+  if (data.length == 0) {
+    res += "<h3>No added stories</h3>";
+  } else {
+    data.forEach(function (i) {
+      res +=
+        '<div><a href="' +
+        i.storyURL +
+        '" target="_blank">link to story {' +
+        index++ +
+        "}</a></div>";
+    });
+  }
+  res += '<h3><a href=" / ">Add Stories</a></h3>';
+  response.send(res);
+});
+
+//post request to add stories
 app.post("/scrape", upload.none(), async (request, response) => {
   let req = request.body;
-  console.log(req);
+  if (req.userName == "" || req.pswd == "") {
+    console.log("Please enter username and password");
+    response.redirect("/error");
+    return;
+  }
+  console.log("Running for " + req.userName);
   try {
     await scrape(req.userName, req.pswd);
   } catch (e) {
     console.log(e);
     response.redirect("/error");
+    return;
   }
 
   response.redirect("/added");
 });
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
